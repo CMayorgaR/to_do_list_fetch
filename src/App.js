9364d2b9289b68de0b5a4b8f6d42e48b7f5c8126
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-05-20 21:31:15 -0400

    fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import { useState } from "react";
 
 function App() {
 
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/cmayorga', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(resp => {
+        console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+        console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+        console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+        return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+    })
+    .then(data => {
+        //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+        console.log("DATA",data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+    })
+    .catch(error => {
+        //manejo de errores
+        console.log(error);
+    });
+
+
+    //__________________________________________________________________________________________________________________________________________________________
   const [list, setList] = useState([]);
 
   const [task, newTask] = useState({});
